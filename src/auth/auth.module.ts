863652de3ab4c90ea1c4043a0773389c32bd7b35
dracu-lah <nevilnicks4321@gmail.com>
2025-02-01 08:56:12 +0530

    added passport auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,14 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JWT_SECRET } from 'src/configs/jwt-secret';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportAuthController } from './passport-auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController, PassportAuthController],
   imports: [
     UsersModule,
     JwtModule.register({
@@ -15,6 +19,7 @@ import { JWT_SECRET } from 'src/configs/jwt-secret';
       secret: JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
+    PassportModule,
   ],
 })
 export class AuthModule {}
